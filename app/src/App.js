5c69dea760a4358a8899f01bd72eb2f58bc5b66d
erindepew <erindepew@gmail.com>
2019-06-24 21:48:34 -0400

    basic styles and layout for app

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+        <header className="header"> Crowd-Funding App </header>
+        <div className="content">
+        <h1>Project Title</h1>
+          <h2>Short description about this project</h2>
+        <img alt="project"/>
+        </div>
+        <div className="sidebar">
+        <h3 className="project-amount">$8,000 pledged of $3,000 goal</h3>
+        <h3 className="project-backers">521 backers</h3>
+        <h3 className="project-deadline">23 days to go</h3>
+        <button className="button">Back This Project</button>
+        </div>
       </div>
     );
   }
